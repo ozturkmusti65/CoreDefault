4c78cfca557cbf0ee78cc527991d0187932a0b66
ozturkmusti65 <ozturk.musti65@gmail.com>
2022-06-20 16:20:38 +0300

    Yazar paneli eklendi. Panel Düzenlendi. 53. videoya gelindi.

--- CoreDefault/Controllers/WriterController.cs
+++ CoreDefault/Controllers/WriterController.cs
@@ -17,5 +17,20 @@ namespace CoreDefault.Web.Controllers
         {
             return View();
         }
+        [AllowAnonymous]
+        public IActionResult Test()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public PartialViewResult WriterNavbarPartial()
+        {
+            return PartialView();
+        }
+        [AllowAnonymous]
+        public PartialViewResult WriterFooterPartial()
+        {
+            return PartialView();
+        }
     }
 }

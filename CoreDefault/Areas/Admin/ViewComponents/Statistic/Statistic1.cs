ad9b6453d450002aa720487281956f10f666c116
ozturkmusti65 <ozturk.musti65@gmail.com>
2022-08-04 17:53:08 +0300

    101. videoya gelindi. İstatistik sayfaları geliştirildi. Geliştirilmeye devam ediyor.

---
+++ CoreDefault/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -0,0 +1,21 @@
+﻿using CoreDefault.BL.Concrete;
+using CoreDefult.DAL.Concrete;
+using CoreDefult.DAL.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CoreDefault.Web.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic1 : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EFBlogRepository());
+        Context c = new Context(); 
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.BlogSayisi = bm.GetList().Count();
+            ViewBag.MesajSayisi = c.Contacts.Count();
+            ViewBag.YorumSayisi = c.Comments.Count();
+            return View();
+        }
+    }
+}
---
+++ CoreDefault/Areas/Admin/ViewComponents/Statistic/Statistic2.cs
@@ -0,0 +1,16 @@
+﻿using CoreDefult.DAL.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CoreDefault.Web.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic2 : ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = c.Blogs.OrderByDescending(x => x.Id).Select(s => s.Title).Take(1).FirstOrDefault();
+            return View();
+        }
+    }
+}

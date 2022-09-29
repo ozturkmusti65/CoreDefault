464246c76e9f5cedc34a7405976bcc7d2cb6513a
ozturkmusti65 <ozturk.musti65@gmail.com>
2022-09-29 11:49:27 +0300

    126. derse gelindi.

--- CoreDefault/Controllers/LoginController.cs
+++ CoreDefault/Controllers/LoginController.cs
@@ -1,8 +1,10 @@
 ﻿using CoreDefault.Entity.Concrete;
+using CoreDefault.Web.Models;
 using CoreDefult.DAL.Concrete;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,36 +13,62 @@ using System.Threading.Tasks;
 
 namespace CoreDefault.Web.Controllers
 {
+    [AllowAnonymous]
     public class LoginController : Controller
     {
-        [AllowAnonymous]
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpPost]
-        [AllowAnonymous]
-        public async Task<IActionResult> Index(Writer p)
+        public async Task<IActionResult> Index(UserSignInViewModel p)
         {
-            Context c = new Context();
-            var datavalue = c.Writers.FirstOrDefault(x => x.Mail == p.Mail && x.Password == p.Password);
-            if (datavalue != null)
+            if (ModelState.IsValid)
             {
-                var claims = new List<Claim>
+                var result = await _signInManager.PasswordSignInAsync(p.UserName, p.Password, false/*Çerezlerde hatırlasın mı ?*/, true/*5 yanlış girişte ban yer kullanıcı*/);
+                if (result.Succeeded)
                 {
-                    new Claim(ClaimTypes.Name, p.Mail),
-                };
-                var userIdentity = new ClaimsIdentity(claims, "a");
-                ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
-                await HttpContext.SignInAsync(principal);
-                return RedirectToAction("Index", "Dashboard");
+                    return RedirectToAction("Index", "Dashboard");
+                }
+                else
+                {
+                    return RedirectToAction("Index","Login");
+                }
             }
-            else
-            {
-                return View();
-            }
+            return View();
         }
+
+
+
+        //[HttpPost]
+        //public async Task<IActionResult> Index(Writer p)
+        //{
+        //    Context c = new Context();
+        //    var datavalue = c.Writers.FirstOrDefault(x => x.Mail == p.Mail && x.Password == p.Password);
+        //    if (datavalue != null)
+        //    {
+        //        var claims = new List<Claim>
+        //        {
+        //            new Claim(ClaimTypes.Name, p.Mail),
+        //        };
+        //        var userIdentity = new ClaimsIdentity(claims, "a");
+        //        ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+        //        await HttpContext.SignInAsync(principal);
+        //        return RedirectToAction("Index", "Dashboard");
+        //    }
+        //    else
+        //    {
+        //        return View();
+        //    }
+        //}
     }
 }
 //Context c = new Context();
---
+++ CoreDefault/Models/UserSignInViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreDefault.Web.Models
+{
+    public class UserSignInViewModel
+    {
+        [Required(ErrorMessage = "Lütfen Kullanıcı Adını Girin")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Şifrenizi Girin")]
+        public string Password { get; set; }
+    }
+}

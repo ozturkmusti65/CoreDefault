410131988ed52771417a211a426cb4fe87375e2e
ozturkmusti65 <ozturk.musti65@gmail.com>
2022-10-24 18:21:00 +0300

    147. ders de 9:58 de kalındı.

---
+++ CoreDefault.jwt_core_proje_kampi/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using CoreDefault.jwt_core_proje_kampi.DAL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDefault.jwt_core_proje_kampi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("action")]
+        public IActionResult Login()
+        {
+            return Created("", new BuildToken().CreateToken());
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult Page1()
+        {
+            return Ok("Sayfa 1 için girişiniz başarılı");
+        }
+    }
+}

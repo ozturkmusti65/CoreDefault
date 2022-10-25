b14633c7bfc5ee6e68457c01c0fc4f518be638b0
ozturkmusti65 <ozturk.musti65@gmail.com>
2022-10-25 11:35:19 +0300

    Tokenlar tamamlandı. 148. derse gelindi.

--- CoreDefault.jwt_core_proje_kampi/Controllers/DefaultController.cs
+++ CoreDefault.jwt_core_proje_kampi/Controllers/DefaultController.cs
@@ -9,14 +9,14 @@ namespace CoreDefault.jwt_core_proje_kampi.Controllers
     [ApiController]
     public class DefaultController : ControllerBase
     {
-        [HttpGet("action")]
+        [HttpGet("[action]")]
         public IActionResult Login()
         {
             return Created("", new BuildToken().CreateToken());
         }
 
         [Authorize]
-        [HttpGet]
+        [HttpGet("[action]")]
         public IActionResult Page1()
         {
             return Ok("Sayfa 1 için girişiniz başarılı");
--- CoreDefault.jwt_core_proje_kampi/DAL/BuildToken.cs
+++ CoreDefault.jwt_core_proje_kampi/DAL/BuildToken.cs
@@ -12,7 +12,7 @@ namespace CoreDefault.jwt_core_proje_kampi.DAL
             var bytes = Encoding.UTF8.GetBytes("aspnetcoreprojekampi");
             SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
             SigningCredentials credentials = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
-            JwtSecurityToken token = new JwtSecurityToken(issuer: "http://localhost", audience: "http://localhost", notBefore: DateTime.Now.AddMinutes(1), signingCredentials: credentials);
+            JwtSecurityToken token = new JwtSecurityToken(issuer: "http://localhost", audience: "http://localhost", notBefore: DateTime.Now, expires: DateTime.Now.AddMinutes(1), signingCredentials: credentials);
             JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
 
             return handler.WriteToken(token);
